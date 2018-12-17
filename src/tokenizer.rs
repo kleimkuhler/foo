95008baada2597b45d3a5c9b4977480a1af2b174
Kevin Leimkuhler <kevin@kleimkuhler.com>
2018-12-16 22:28:25 -0800

    Tokenize subset of scheme syntax

--- src/tokenizer.rs
+++ src/tokenizer.rs
@@ -1,17 +1,17 @@
 #![allow(dead_code)]
 
-use std::{fmt, iter::Peekable, result::Result as StdResult, str::CharIndices};
+use std::{iter::Peekable, result::Result as StdResult, str::CharIndices};
 
-fn is_identity(ch: char) -> bool {
-    match ch {
-        'a'..='z' | 'A'..='Z' | '_' => true,
+fn is_delimiter(c: char) -> bool {
+    match c {
+        '(' | ')' | ';' | '"' | '\'' | '`' | '|' | '[' | ']' | '{' | '}' => true,
         _ => false,
     }
 }
 
 #[derive(Debug, PartialEq)]
 enum LexerError {
-    UnexpectedCharacter(char),
+    NonTerminatedString(String),
 }
 
 type Result<T, E> = StdResult<T, E>;
@@ -19,25 +19,17 @@ type Result<T, E> = StdResult<T, E>;
 #[derive(Debug, PartialEq)]
 pub enum Token<'input> {
     // Data
-    Identity(&'input str),
+    Identifier(&'input str),
+    Literal(&'input str),
+    StringLiteral(&'input str),
 
     // Symbols
-    BackSlash,
-    Dot,
-    LeftParen,
-    RightParen,
-}
-
-impl<'input> fmt::Display for Token<'input> {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        match self {
-            Token::Identity(ident) => write!(f, "{}", ident),
-            Token::BackSlash => write!(f, "\\"),
-            Token::Dot => write!(f, "."),
-            Token::LeftParen => write!(f, "("),
-            Token::RightParen => write!(f, ")"),
-        }
-    }
+    LBracket,
+    RBracket,
+    LCurlyBrace,
+    RCurlyBrace,
+    LParen,
+    RParen,
 }
 
 struct Tokenizer<'input> {
@@ -57,8 +49,8 @@ impl<'input> Tokenizer<'input> {
     where
         F: FnMut(char) -> bool,
     {
-        while let Some((end, ch)) = self.chars.peek().cloned() {
-            if proceed(ch) {
+        while let Some((end, c)) = self.chars.peek().cloned() {
+            if proceed(c) {
                 self.chars.next();
             } else {
                 return &self.input[start..end];
@@ -67,24 +59,70 @@ impl<'input> Tokenizer<'input> {
 
         &self.input[start..self.input.len()]
     }
+
+    fn contiguous_slice(&mut self, start: usize) -> &'input str {
+        self.read_while(start, |c| !is_delimiter(c) && !c.is_whitespace())
+    }
+
+    fn consume_identifier(&mut self, start: usize) -> Result<Token<'input>, LexerError> {
+        let identifier = self.contiguous_slice(start);
+        Ok(Token::Identifier(identifier))
+    }
+
+    fn consume_literal(&mut self, start: usize) -> Result<Token<'input>, LexerError> {
+        let literal = self.contiguous_slice(start);
+        Ok(Token::Literal(literal))
+    }
+
+    fn consume_string(&mut self, start: usize) -> Result<Token<'input>, LexerError> {
+        let slice = self.read_while(start + 1, |c| c != '"' && c != '\n');
+
+        if slice.len() != 1 {
+            if let Some((_, peek)) = self.chars.peek().cloned() {
+                return match peek {
+                    '"' => {
+                        self.chars.next();
+                        Ok(Token::StringLiteral(slice))
+                    }
+                    _ => Err(LexerError::NonTerminatedString(slice.to_string())),
+                };
+            }
+        }
+
+        Err(LexerError::NonTerminatedString(slice.to_string()))
+    }
+
+    fn consume_line_comment(&mut self, start: usize) {
+        self.read_while(start, |c| c != '\n');
+    }
 }
 
 impl<'input> Iterator for Tokenizer<'input> {
     type Item = Result<Token<'input>, LexerError>;
 
     fn next(&mut self) -> Option<Self::Item> {
-        while let Some((i, ch)) = self.chars.next() {
-            return Some(match ch {
-                '(' => Ok(Token::LeftParen),
-                ')' => Ok(Token::RightParen),
-                '\\' => Ok(Token::BackSlash),
-                '.' => Ok(Token::Dot),
-                ch if is_identity(ch) => {
-                    let identity = self.read_while(i, |ch| is_identity(ch));
-                    Ok(Token::Identity(identity))
+        while let Some((i, c)) = self.chars.next() {
+            return Some(match c {
+                // Delimiters
+                '[' => Ok(Token::LBracket),
+                ']' => Ok(Token::RBracket),
+                '{' => Ok(Token::LCurlyBrace),
+                '}' => Ok(Token::RCurlyBrace),
+                '(' => Ok(Token::LParen),
+                ')' => Ok(Token::RParen),
+
+                // Data
+                '\'' => self.consume_literal(i),
+                '"' => self.consume_string(i),
+
+                // Trivia
+                ';' => {
+                    self.consume_line_comment(i);
+                    continue;
                 }
-                ch if ch.is_whitespace() => continue,
-                _ => Err(LexerError::UnexpectedCharacter(ch)),
+                c if c.is_whitespace() => continue,
+
+                _ => self.consume_identifier(i),
             });
         }
 
@@ -101,58 +139,29 @@ mod tests {
     }
 
     #[test]
-    fn identities() {
+    fn trivia() {
+        assert_eq!(tokenize("; this is a line comment"), vec![]);
         assert_eq!(
-            tokenize("foo bar baz"),
-            &[
-                Ok(Token::Identity("foo")),
-                Ok(Token::Identity("bar")),
-                Ok(Token::Identity("baz"))
-            ]
+            tokenize("; this is a line comment\n \t; this is another line comment"),
+            vec![]
         )
     }
 
     #[test]
-    fn symbols() {
+    fn valid() {
         assert_eq!(
-            tokenize(r"( ( . \ ( \ ) . ) )"),
+            tokenize(r#"(list"Hi"name(+ 1 2))"#),
             &[
-                Ok(Token::LeftParen),
-                Ok(Token::LeftParen),
-                Ok(Token::Dot),
-                Ok(Token::BackSlash),
-                Ok(Token::LeftParen),
-                Ok(Token::BackSlash),
-                Ok(Token::RightParen),
-                Ok(Token::Dot),
-                Ok(Token::RightParen),
-                Ok(Token::RightParen)
-            ]
-        )
-    }
-
-    #[test]
-    fn program() {
-        assert_eq!(
-            tokenize(r"(\ foo . \ y . foo y) (\ bar . bar) w"),
-            &[
-                Ok(Token::LeftParen),
-                Ok(Token::BackSlash),
-                Ok(Token::Identity("foo")),
-                Ok(Token::Dot),
-                Ok(Token::BackSlash),
-                Ok(Token::Identity("y")),
-                Ok(Token::Dot),
-                Ok(Token::Identity("foo")),
-                Ok(Token::Identity("y")),
-                Ok(Token::RightParen),
-                Ok(Token::LeftParen),
-                Ok(Token::BackSlash),
-                Ok(Token::Identity("bar")),
-                Ok(Token::Dot),
-                Ok(Token::Identity("bar")),
-                Ok(Token::RightParen),
-                Ok(Token::Identity("w"))
+                Ok(Token::LParen),
+                Ok(Token::Identifier("list")),
+                Ok(Token::StringLiteral("Hi")),
+                Ok(Token::Identifier("name")),
+                Ok(Token::LParen),
+                Ok(Token::Identifier("+")),
+                Ok(Token::Identifier("1")),
+                Ok(Token::Identifier("2")),
+                Ok(Token::RParen),
+                Ok(Token::RParen)
             ]
         )
     }
